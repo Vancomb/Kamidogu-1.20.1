f390977424c95ceac7b53a52f61c16b251ccda48
Vancomb <Vancomb97@gmail.com>
2024-09-02 14:15:48 -0400

    Registered `AbstractHumanoidEntity` class, which other basic human entities will inherit from.
    Created and registered ModItems and Creative Tab.

--- src/main/java/net/vancomb/kamidogu/Kamidogu.java
+++ src/main/java/net/vancomb/kamidogu/Kamidogu.java
@@ -1,16 +1,11 @@
 package net.vancomb.kamidogu;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.vancomb.kamidogu.entity.ModEntityTypes;
